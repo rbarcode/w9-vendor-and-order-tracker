4a905ed3ee797e78ea64aa3aca8a099634a4105c
Richard Barbour <richy07@gmail.com>
2023-09-29 11:56:18 -0700

    Add HomeController and home Index.cs, and add work-in-progress VendorsController and vendors Index.cs and New.cs.

---
+++ VAOTracker.Solution/VAOTracker/Controllers/VendorsController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using VAOTracker.Models;
+
+namespace VAOTracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+  }
+}
